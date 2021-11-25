24a6b6ef4885093601f6be4747786dd3c1203faf
surajubale <92155720+surajubale@users.noreply.github.com>
2021-11-26 00:00:26 +0530

    Update index.js

--- src/Components/Intro/index.js
+++ src/Components/Intro/index.js
@@ -2,9 +2,11 @@ import React from "react";
 
 
 const Intro =  props => {
+    return (
     <p className="App-intro">
         Our first functional component
     </p>
-};
+    );
+}
 
 export default Intro;
